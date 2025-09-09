973d91fef3fa059d107386f72b3d050c2619b3da
UmVirt development team <dev@umvirt.com>
2025-09-09 16:44:29 +0900

    add "m/y" expected value support in checks

--- cmp.c
+++ cmp.c
@@ -5,15 +5,31 @@ int main(int argc, char * argv[]){
 if(argc != 3){
         printf("ERROR");
     }else{
-	if (strcmp(argv[1],argv[2])==0){
-                    printf("OK");
-//            else if(strcmp(argv[1], printf('OK'))!=0)
-//                    printf('FAIL')
-            }else{
+        //if argumets are equal
+        if (strcmp(argv[1],argv[2])==0){
+            printf("OK");
+            return 0;
+        //in other cases
+        }else{
+                    //if option value is should be a module or part of kernel
+                    if (strcmp(argv[2],"m/y")==0){
+
+                        //allow module value
+                        if (strcmp(argv[1],"m")==0){
+                            printf("OK");
+                            return 0;
+                        }
+
+                        //allow part of kernel value
+                        if (strcmp(argv[1],"y")==0){
+                            printf("OK");
+                            return 0;
+                        }
+                    }
+
+                    //in other cases
                     printf("FAIL");
-	}
-}
-//printf(argv[0]);
-//printf("OK");
-return 0;
+            }
+        }
+    return 0;
 }
